1ec7aacf2971c65737d56c918e819420dd37ef0b
David Alfredo Hernandez Ramirez <davidhr2408@gmail.com>
2023-11-15 17:54:46 -0600

    pagina publica

--- Hospital/Hospital/settings.py
+++ Hospital/Hospital/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,"Hospital/Templates")
+            os.path.join(BASE_DIR,"Hospital/templates")
         ],
         'APP_DIRS': True,
         'OPTIONS': {
--- Hospital/sucursales/views.py
+++ Hospital/sucursales/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+def index(request):
+    return render(request,"index.html")
+
 def sucursales(request):
     return HttpResponse("<h1>Bienvenido al hospital</h1>")
-
-def index(request):
-    return HttpResponse("<h1>Bienvenido al menu de sucursales</h1>")
