6b22df84707fae5d3017c9678031ba8b2f94f889
Emmanuel Gaona Acevedo <emm.gaona@gmail.com>
2023-11-15 19:40:13 -0600

    cambios

--- Hospital/sucursales/models.py
+++ Hospital/sucursales/models.py
@@ -6,7 +6,7 @@ class Sucursalesinfo(models.Model):
     nombre_sucursal = models.CharField(max_length=60,null=True)
     ubicacion = models.CharField(max_length=60,null=True)
     telefono =models.CharField(max_length=10,null=True)
-    capacidad_pacientes = models.FloatField(null=True)
+    capacidad_pacientes = models.IntegerField(null=True)
     descripcion_servicios = models.CharField(max_length=60)
 
     def __str__(self):
